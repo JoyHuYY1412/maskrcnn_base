898ae9ba13ecba89e08c6573f2898c39922eb9b1
JoyHuYY1412 <hxt1412@mail.ustc.edu.cn>
2020-03-17 15:37:57 +0800

    Update get_finetune_model.sh

--- get_finetune_model.sh
+++ get_finetune_model.sh
@@ -1,1 +1,1 @@
-python get_finetune_model.py -i './model_0055000.pth' -o './model_0055000_for_finetune.pth'
+python get_finetune_model.py -i './model_0040000.pth' -o './model_0040000_for_finetune.pth'
