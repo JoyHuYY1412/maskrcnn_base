02f62a891e2137a542be65ced64cf39244f5610b
JoyHuYY1412 <hxt1412@mail.ustc.edu.cn>
2020-03-17 15:38:28 +0800

    Update get_finetune_model.py

--- get_finetune_model.py
+++ get_finetune_model.py
@@ -13,19 +13,19 @@ def main(argv):
     state_dict = torch.load(model_path, map_location="cpu")
     
     model = state_dict['model']
-    cls_weight_new = torch.Tensor(271+160*3, 1024)
+    cls_weight_new = torch.Tensor(271+160*4, 1024)
     nn.init.normal_(cls_weight_new, std=0.01)
-    cls_weight_new[:271+160*2] = model['module.roi_heads.box.predictor.cls_score.weight']
+    cls_weight_new[:271+160*3] = model['module.roi_heads.box.predictor.cls_score.weight']
     model['module.roi_heads.box.predictor.cls_score.weight'] = cls_weight_new
 
-    reg_weight_new = torch.Tensor((271+160*3)*4, 1024)
+    reg_weight_new = torch.Tensor((271+160*4)*4, 1024)
     nn.init.normal_(reg_weight_new, std=0.001)
-    reg_weight_new[:(271+160*2)*4] = model['module.roi_heads.box.predictor.bbox_pred.weight']
+    reg_weight_new[:(271+160*3)*4] = model['module.roi_heads.box.predictor.bbox_pred.weight']
     model['module.roi_heads.box.predictor.bbox_pred.weight'] = reg_weight_new
     
-    reg_bias_new = torch.Tensor((271+160*3)*4)
+    reg_bias_new = torch.Tensor((271+160*4)*4)
     nn.init.constant_(reg_bias_new, 0)
-    reg_bias_new[:(271+160*2)*4] = model['module.roi_heads.box.predictor.bbox_pred.bias']
+    reg_bias_new[:(271+160*3)*4] = model['module.roi_heads.box.predictor.bbox_pred.bias']
     model['module.roi_heads.box.predictor.bbox_pred.bias'] = reg_bias_new
 
     print("Also deleting optimizer, scheduler, and iteration entries")
